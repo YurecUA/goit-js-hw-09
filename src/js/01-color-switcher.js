286b00cbdf466c2947a36aea67d66636a7825408
YurecUA <81627173+YurecUA@users.noreply.github.com>
2022-11-18 00:00:41 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let itemID = null;
+let changeBgnColorBtnOn = false;
+
+const onStartBtnClick = () => {
+  if (changeBgnColorBtnOn) {
+    return;
+  }
+  changeBgnColorBtnOn = true;
+  itemID = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const onStopBtnClick = () => {
+  clearInterval(itemID);
+  changeBgnColorBtnOn = false;
+};
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
